30aab316a8815b7858cb2dee34a21a0acc7f9e80
Tonyfox58 <arthon58@hotmail.com>
2025-01-16 14:56:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,72 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-/*
-struct Pessoa {
-    char nome[50];
-    int idade;
-    float altura;
-};
-
-int main() {
-    struct Pessoa pessoa;
-
-    printf("Digite o nome: ");
-    fgets(pessoa.nome, sizeof(pessoa.nome), stdin);
-
-        pessoa.nome[strcspn(pessoa.nome, "\n")] = 0;
-
-    printf("Digite a idade: ");
-    scanf("%d", &pessoa.idade);
-
-    printf("Digite a altura: ");
-    scanf("%f", &pessoa.altura);
-
-    printf("\nNome: %s\nIdade: %d\nAltura: %.2f\n", pessoa.nome, pessoa.idade, pessoa.altura);
-
-    return 0;
-}
-
-*/
-
-/*
-struct Ponto {
-
-        int x;
-   		int y;
-};
-int main() {
-
-  		struct Ponto p1 = {10, 20};
-
-    		printf("Ponto: (%d, %d)\n", p1.x, p1.y);
-    		return 0;
-}
-*/
-/*
-enum DiaDaSemana {
-    DOMINGO,
-    SEGUNDA,
-    TERCA,
-    QUARTA,
-    QUINTA,
-    SEXTA,
-    SABADO
-};
-
-int main() {
-    enum DiaDaSemana hoje = QUARTA;
-
-    if (hoje == QUARTA) {
-        printf("Hoje é quarta-feira!\n");
-    }
-
-    return 0;
-}
-*/
-
-
-
-#include <stdio.h>
 #include <time.h>
 
 
@@ -89,7 +21,7 @@ const char* nome_do_mes(int mes) {
     switch (mes) {
         case JANEIRO: return "Janeiro";
         case FEVEREIRO: return "Fevereiro";
-        case MARCO: return "Março";
+        case MARCO: return "MarÃ§o";
         case ABRIL: return "Abril";
         case MAIO: return "Maio";
         case JUNHO: return "Junho";
@@ -99,7 +31,7 @@ const char* nome_do_mes(int mes) {
         case OUTUBRO: return "Outubro";
         case NOVEMBRO: return "Novembro";
         case DEZEMBRO: return "Dezembro";
-        default: return "Mês inválido";
+        default: return "MÃªs invÃ¡lido";
     }
 }
 int main() {
@@ -110,7 +42,7 @@ int main() {
 
     tm_info = localtime(&t);
     if (tm_info == NULL) {
-        printf("Erro ao obter informações de tempo.\n");
+        printf("Erro ao obter informaÃ§Ãµes de tempo.\n");
         return 1;
     }
 
